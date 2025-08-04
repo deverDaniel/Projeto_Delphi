f6e50096652b2a3036ffb1f35d0f07f1b8763c84
DanielFerreira <dever.daniel.asf@gmail.com>
2025-08-04 15:56:21 -0300

    adicionando class professor

---
+++ uProfessores.pas
@@ -0,0 +1,54 @@
+unit uProfessores;
+
+interface
+  type Professores = class
+    private
+      id: Integer;
+      nome: String;
+      cpf: String;
+    public
+      function getId: Integer;
+      function getNome: String;
+      procedure setNome(nome: String);
+      function getCpf: String;
+      procedure setCpf(cpf: String);
+      constructor Create (id: Integer; nome: String);
+
+  end;
+implementation
+
+{ Estudantes }
+
+constructor Professores.Create(id: Integer; nome: String);
+begin
+  self.id:= id;
+  self.nome:= nome;
+
+end;
+
+function Professores.getCpf: String;
+begin
+  result:= self.cpf;
+end;
+
+function Professores.getId: Integer;
+begin
+  result:= self.id;
+end;
+
+function Professores.getNome: String;
+begin
+  result:= self.nome
+end;
+
+procedure Professores.setCpf(cpf: String);
+begin
+  self.cpf:= cpf;
+end;
+
+procedure Professores.setNome(nome: String);
+begin
+  self.nome:= nome
+end;
+
+end.
