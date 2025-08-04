1eceb720bc3b8d465fd033ef067ef9f55159bad6
DanielFerreira <dever.daniel.asf@gmail.com>
2025-08-04 15:52:55 -0300

    adicioando classe estudante

--- uEstudante.pas
+++ uEstudante.pas
@@ -1,7 +1,41 @@
 unit uEstudante;
 
 interface
+  type Estudantes = class
+    private
+      id: Integer;
+      nome: String;
+    public
+      function getId: Integer;
+      function getNome: String;
+      procedure setNome(nome: String);
+      constructor Create (id: Integer; nome: String);
 
+  end;
 implementation
+
+{ Estudantes }
+
+constructor Estudantes.Create(id: Integer; nome: String);
+begin
+  self.id:= id;
+  self.nome:= nome;
+
+end;
+
+function Estudantes.getId: Integer;
+begin
+  result:= self.id;
+end;
+
+function Estudantes.getNome: String;
+begin
+  result:= self.nome
+end;
+
+procedure Estudantes.setNome(nome: String);
+begin
+  self.nome:= nome
+end;
 
 end.
